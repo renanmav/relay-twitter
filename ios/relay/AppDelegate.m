5e066995cf8817ce482a9b373cc4eff072f6b2e2
Renan Machado <renan.mav@hotmail.com>
2019-08-16 19:31:33 -0300

    feat(ios): configure splash screen

--- ios/relay/AppDelegate.m
+++ ios/relay/AppDelegate.m
@@ -10,6 +10,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import "SplashScreen.h"
 
 @implementation AppDelegate
 
@@ -27,6 +28,7 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [SplashScreen show];
   return YES;
 }
 
