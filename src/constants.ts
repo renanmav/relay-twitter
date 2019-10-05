2c393e5a9286fdce7efd856be8a5fb47b1ea1953
Renan Machado <renan.mav@hotmail.com>
2019-10-05 12:49:54 -0300

    change server url

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,11 @@
 export const TT_TOKEN = "TT_TOKEN";
 export const TT_ITEMS_PER_PAGE = 10;
-export const GRAPHQL_ANDROID_ENDPOINT = "http://10.0.3.2:5000/graphql";
-export const GRAPHQL_IOS_ENDPOINT = "http://server.renanmav.now.sh/graphql";
+export const GRAPHQL_ANDROID_ENDPOINT =
+  "https://relay-twitter-server.herokuapp.com/graphql";
+export const GRAPHQL_IOS_ENDPOINT =
+  "https://relay-twitter-server.herokuapp.com/graphql";
 export const GRAPHQL_SUBSCRIPTION_IOS_ENDPOINT =
-  "ws://server.renanmav.now.sh/subscriptions";
+  "ws://relay-twitter-server.herokuapp.com/subscriptions";
 export const GRAPHQL_SUBSCRIPTION_ANDROID_ENDPOINT =
-  "ws://10.0.3.2:5000/subscriptions";
+  "ws://relay-twitter-server.herokuapp.com/subscriptions";
 export const TT_THEME = "TT_THEME";
